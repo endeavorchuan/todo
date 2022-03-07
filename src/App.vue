c605546e24567036710faf2ce59dd42a88cd28c3
川 <chc82347794@163.com>
2022-03-07 18:06:29 +0800

    vue2 --> vue3变更

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <h1>count: {{ countRef }}</h1>
+  <p>
+    <button @click="decrease">decrease</button>
+    <button @click="increase">increase</button>
+  </p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref} from "vue";
+
+function useCount() {
+  let countRef = ref(0);   // 此处的数据具有响应式，官方建议使用ref创建时，变量名后添加后缀
+  console.log(countRef);   // ref函数将数字封装到对象里，所以count不是数字，而是一个对象
+
+  const increase = () => {
+    countRef.value ++;
+  };
+
+  const decrease = () => {
+    countRef.value --;
+  };
+
+  return {
+    countRef,
+    increase,
+    decrease
+  };
+}
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  // 这个函数会自动执行，在所有生命周期钩子函数之前调用
+  setup() {
+    // console.log("在所有生命周期钩子函数之前调用")
+    // console.log(this);    // 此处的this为undefined
+
+    // let count = 0;
+    // const increase = () => {
+    //   count ++;    // 此处增加，并未引起重新渲染，即数据不具有响应式
+    // };
+
+    // count本身是一个对象，但直接访问count也可以读取count.value的值，是因为count访问的是组件代理对象，
+    // 组件代理对象访问组件实例中的count.value
+
+    // 返回的对象中的所有属性，都会到组件实例中去
+    return {
+      ...useCount()
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 
-createApp(App).mount('#app')
+// vue2
+// const app = new Vue(options)
+// app.$mount("#app");
+
+// vue2使用插件   Vue.use()
+// vue3使用插件   app.use
+
+// vue3
+// 不存在构造函数
+
+const app = createApp(App);     // 返回一个vue实例对象，提供经常会用到的方法
+app.mount("#app");    // 挂载到某一个元素里
+// console.log(app);
