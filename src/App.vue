84aeafb5bf2a209f83b8f8d3f078faf8e939f9df
川 <chc82347794@163.com>
2022-03-08 10:38:51 +0800

    基本样式

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,63 @@
 <template>
-  <h1>count: {{ countRef }}</h1>
-  <p>
-    <button @click="decrease">decrease</button>
-    <button @click="increase">increase</button>
-  </p>
+  <div id="app">
+    <section class="todoapp">
+      <header class="header">
+        <h1>todos</h1>
+        <input
+          class="new-todo"
+          autofocus=""
+          autocomplete="off"
+          placeholder="What needs to be done?"
+        />
+      </header>
+      <section class="main">
+        <input id="toggle-all" class="toggle-all" type="checkbox" />
+        <label for="toggle-all">Mark all as complete</label>
+        <ul class="todo-list">
+          <li class="todo">
+            <div class="view">
+              <input class="toggle" type="checkbox" />
+              <label>学习composition api</label>
+              <button class="destroy"></button>
+            </div>
+            <input class="edit" type="text" />
+          </li>
+          <li class="todo">
+            <div class="view">
+              <input class="toggle" type="checkbox" />
+              <label>投递50封简历</label>
+              <button class="destroy"></button>
+            </div>
+            <input class="edit" type="text" />
+          </li>
+          <li class="todo">
+            <div class="view">
+              <input class="toggle" type="checkbox" />
+              <label>上午10:30 参加面试</label>
+              <button class="destroy"></button>
+            </div>
+            <input class="edit" type="text" />
+          </li>
+        </ul>
+      </section>
+      <footer class="footer">
+      <span class="todo-count">
+        <strong>3</strong>
+        <span>items left</span>
+      </span>
+        <ul class="filters">
+          <li><a href="#/all" class="selected">All</a></li>
+          <li><a href="#/active" class="">Active</a></li>
+          <li><a href="#/completed" class="">Completed</a></li>
+        </ul>
+        <button class="clear-completed" style="display: none">
+          Clear completed
+        </button>
+      </footer>
+    </section>
+  </div>
 </template>
 
 <script>
-import {ref} from "vue";
-
-function useCount() {
-  let countRef = ref(0);   // 此处的数据具有响应式，官方建议使用ref创建时，变量名后添加后缀
-  console.log(countRef);   // ref函数将数字封装到对象里，所以count不是数字，而是一个对象
-
-  const increase = () => {
-    countRef.value ++;
-  };
-
-  const decrease = () => {
-    countRef.value --;
-  };
-
-  return {
-    countRef,
-    increase,
-    decrease
-  };
-}
-
-export default {
-  // 这个函数会自动执行，在所有生命周期钩子函数之前调用
-  setup() {
-    // console.log("在所有生命周期钩子函数之前调用")
-    // console.log(this);    // 此处的this为undefined
-
-    // let count = 0;
-    // const increase = () => {
-    //   count ++;    // 此处增加，并未引起重新渲染，即数据不具有响应式
-    // };
-
-    // count本身是一个对象，但直接访问count也可以读取count.value的值，是因为count访问的是组件代理对象，
-    // 组件代理对象访问组件实例中的count.value
-
-    // 返回的对象中的所有属性，都会到组件实例中去
-    return {
-      ...useCount()
-    }
-  }
-}
+  export default {}
 </script>
